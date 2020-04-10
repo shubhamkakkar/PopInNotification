d75a778bbd1600ee6ae9693e6ab56f95cdc3955f
shubhamkakkar <shubham.kakkar98@gmail.com>
2020-04-10 19:45:33 +0530

    native mofule bug resolved due to removal of gesture handler

--- android/app/src/main/java/com/popinnotification/MainActivity.java
+++ android/app/src/main/java/com/popinnotification/MainActivity.java
@@ -1,9 +1,5 @@
 package com.popinnotification;
-import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.ReactRootView;
 import com.facebook.react.ReactActivity;
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
-
 public class MainActivity extends ReactActivity {
 
   /**
@@ -13,14 +9,5 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "PopInNotification";
-  }
-  @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegate(this, getMainComponentName()) {
-      @Override
-      protected ReactRootView createRootView() {
-        return new RNGestureHandlerEnabledRootView(MainActivity.this);
-      }
-    };
   }
 }
